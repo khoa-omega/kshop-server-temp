73eeb46cb9735c6c24c4a458967a61bb4cb2ac9c
khoa-omega <nvkhoa05@gmail.com>
2024-01-04 20:32:41 +0700

    [Add] KShop Server

--- src/main/java/com/vti/kshop/KShopApplication.java
+++ src/main/java/com/vti/kshop/KShopApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KshopApplication {
-
+public class KShopApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(KshopApplication.class, args);
+		SpringApplication.run(KShopApplication.class, args);
 	}
-
 }
--- src/test/java/com/vti/kshop/KShopApplicationTests.java
+++ src/test/java/com/vti/kshop/KShopApplicationTests.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KshopApplicationTests {
-
+class KShopApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
